6c66991e0a81968b439c2ad787bf169c911275ef
ggnryr <ggnryr@yahoo.com>
2019-12-03 04:16:27 +0700

    update user

--- original/dist/js/main.js
+++ original/dist/js/main.js
@@ -86,6 +86,29 @@ $(document).ready(function() {
 		jQuery(this).parent('li').addClass('active-tab').siblings().removeClass('active-tab');
 
 		e.preventDefault();
-	});
+  });
+  
+  // Upload Foto
+  var readURL = function(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.profile-pic img').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
+$(".file-upload").on('change', function(){
+    readURL(this);
 });
+
+$(".upload-button").on('click', function() {
+   $(".file-upload").click();
+});
+});
+
 
